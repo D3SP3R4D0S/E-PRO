a32bcc3f4f80cdf5c439b45b9ec1204886a0e364
Gyeongsik Kim <admin@gyungdal.cc>
2022-05-01 23:11:58 +0900

    fix csrf

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(csrf({ cookie: true }))
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(csrf({ cookie: true }));
 
 app.use('/', indexRouter);
 app.use('/', projectRouter);
