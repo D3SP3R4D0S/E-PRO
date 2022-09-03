20e77aa577972783f3312ecd1f649ca1787b83b7
D3SP3R4D0S <lee@inha.edu>
2022-09-03 17:42:21 +0900

    fix: re design project.js

--- routes/project.js
+++ routes/project.js
@@ -13,6 +13,7 @@ connection.connect(function(err){
 });
 connection.close
 
+// projects // mysql
 router.get('/projects', function(req, res, next) {
     let idnum = req.session.idn
     if(req.session.user){
@@ -30,44 +31,48 @@ router.get('/projects', function(req, res, next) {
         res.redirect('login')
     }
 });
-router.get('/projectadd', function(req, res, next) {
-    if(req.session.user){
-        res.render('main/projects/projectadd',{csrfToken:req.csrfToken(),name:req.session.user});
-    }else{
-        res.redirect('login')
-    }
-});
 
-router.post('/projectadd', function(req, res, next) {
-    let rb = req.body
-    if(req.session.user){
-        let sql = "INSERT INTO finance.project(title, description, creator)VALUES(?,?,?);"
-        let params = [rb.title, rb.description, req.session.idn];
-        connection.query(sql,params,function (err, results, fields) {
-            if(err){
-                console.log(err);
-            }else{
-                connection.query("SELECT LAST_INSERT_ID() as id;", function (err, results, fields){
-                    if(err){
-                        console.log(err);
-                    }else{
-                        let pid = results[0].id
-                        let sql = "INSERT INTO finance.project_member (projectid, memberid) VALUES (?,?);"
-                        connection.query(sql,[pid,req.session.idn], function (err, results, fields) {
-                            if(err){
-                                console.log(err);
-                            }else{
-                                res.redirect('/projects');
-                            }
-                        });
-                    }
-                })
-            }
-        });
-    }else{
-        res.redirect('login')
-    }
-});
+// add project
+router.route('/projectadd')
+    // no requirement
+    .get(function(req, res, next) {
+        if(req.session.user){
+            res.render('main/projects/projectadd',{csrfToken:req.csrfToken(),name:req.session.user});
+        }else{
+            res.redirect('login')
+        }
+    })
+    // projectadd // mysql
+    .post(function(req, res, next) {
+        let rb = req.body
+        if(req.session.user){
+            let sql = "INSERT INTO finance.project(title, description, creator)VALUES(?,?,?);"
+            let params = [rb.title, rb.description, req.session.idn];
+            connection.query(sql,params,function (err, results, fields) {
+                if(err){
+                    console.log(err);
+                }else{
+                    connection.query("SELECT LAST_INSERT_ID() as id;", function (err, results, fields){
+                        if(err){
+                            console.log(err);
+                        }else{
+                            let pid = results[0].id
+                            let sql = "INSERT INTO finance.project_member (projectid, memberid) VALUES (?,?);"
+                            connection.query(sql,[pid,req.session.idn], function (err, results, fields) {
+                                if(err){
+                                    console.log(err);
+                                }else{
+                                    res.redirect('/projects');
+                                }
+                            });
+                        }
+                    })
+                }
+            });
+        }else{
+            res.redirect('login')
+        }
+    });
 
 router.post('/projectdetail', function(req, res, next) {
     req.session.pid = req.body.pid
@@ -113,22 +118,46 @@ router.post('/projectdetail', function(req, res, next) {
     }
 });
 
-router.get('/projectaddtask', function(req, res, next) {
+// #########################################################
+// ADD MEMBER ( SEARCH & ADD )
+// #########################################################
+
+// add member
+router.route('/projectaddmember')
+    .get(function(req, res, next) {
+        if(req.session.user){
+            let id = req.query.memberid
+            if(!id) id = '';
+            let sql = "SELECT number, id, name FROM nodedb.account where id = ?;"
+            connection.query(sql,id,function (err, result, fields) {
+                if (err) {console.log(err);
+                } else {
+                    res.render('main/projects/projectaddmember', {id, result,csrfToken:req.csrfToken(), name: req.session.user});
+                }
+            });
+        }else{
+            res.redirect('login')
+        }
+    })
+    .post(function(req, res, next) {
     if(req.session.user){
-        res.render('main/projects/projectaddtask',{csrfToken:req.csrfToken(),name:req.session.user});
-    }else{
-        res.redirect('login')
-    }
-});
-router.post('/projectaddtask', function(req, res, next) {
-    let rb = req.body
-    if(req.session.user){
-        let sql = "INSERT INTO finance.project_task(projectid, tasktitle, creator, duedate, detail)VALUES(?,?,?,?,?);"
-        let params = [req.session.pid, rb.tasktitle, req.session.idn, rb.duedate, rb.detail];
-        console.log(params);
+        let rb = req.body
+        let sql = "SELECT * FROM project_member WHERE projectid = ? AND memberid = ?"
+        let params = [req.session.pid, rb.id]
         connection.query(sql,params,function (err, results, fields) {
-            if(err){
-                console.log(err);
+            if(err) {
+                throw err;
+            }else if(results.length === 0){
+                console.log(results.length)
+                sql = "INSERT INTO project_member(projectid, memberid)VALUES(?,?);"
+                params = [req.session.pid, rb.id];
+                connection.query(sql,params,function (err, results, fields) {
+                    if(err){
+                        console.log(err);
+                    }else{
+                        res.redirect('/projectdetail?pid='+req.session.pid);
+                    }
+                });
             }else{
                 res.redirect('/projectdetail?pid='+req.session.pid);
             }
@@ -136,7 +165,12 @@ router.post('/projectaddtask', function(req, res, next) {
     }else{
         res.redirect('login')
     }
-});
+})
+
+// #########################################################
+// TASK REQUEST PART
+// #########################################################
+
 
 // project/taskdetail // knex raw
 router.route('/projecttaskdetail')
@@ -153,7 +187,7 @@ router.route('/projecttaskdetail')
             knex.raw(sql,[taskid, pid, taskid])
                 .then((rawres)=> {
                     let results = rawres[0]
-                    res.render('main/projects/projecttaskdetail', {
+                    res.render('main/projects/task/projecttaskdetail', {
                         task: results[0][0],
                         pmember: results[1],
                         csrfToken: req.csrfToken(),
@@ -189,15 +223,42 @@ router.route('/projecttaskdetail')
         }else{
             res.redirect('login')
         }
-    });
+    })
+
+router.route('/projectaddtask')
+    .get(function(req, res, next) {
+    if(req.session.user){
+        res.render('main/projects/task/projectaddtask',{csrfToken:req.csrfToken(),name:req.session.user});
+    }else{
+        res.redirect('login')
+    }
+})
+    .post(function(req, res, next) {
+    let rb = req.body
+    if(req.session.user){
+        let sql = "INSERT INTO finance.project_task(projectid, tasktitle, creator, duedate, detail)VALUES(?,?,?,?,?);"
+        let params = [req.session.pid, rb.tasktitle, req.session.idn, rb.duedate, rb.detail];
+        console.log(params);
+        connection.query(sql,params,function (err, results, fields) {
+            if(err){
+                console.log(err);
+            }else{
+                res.redirect('/projectdetail?pid='+req.session.pid);
+            }
+        });
+    }else{
+        res.redirect('login')
+    }
+})
+
 // project / task / commentadd // knex
 router.post('/taskaddcomment', function(req, res, next) {
     let rb = req.body
     if(req.session.user){
         if(rb.status!=''){
             knex('project_task').update({
-                    status:rb.status
-                })
+                status:rb.status
+            })
                 .where('taskid', req.session.tid)
                 .catch((err)=>{
                     console.log(err)
@@ -226,7 +287,6 @@ router.post('/taskaddcomment', function(req, res, next) {
     }
 });
 
-
 //projectaddtask // knex
 router.post('/projectaddtask', function(req, res, next) {
     let rb = req.body
@@ -237,7 +297,7 @@ router.post('/projectaddtask', function(req, res, next) {
             creator:req.session.idn,
             duedate:rb.duedate,
             detail:rb.detail
-            })
+        })
             .then((results)=>{console.log(results)})
             .catch((err)=>{console.log(err)})
         res.redirect('/projectdetail?pid='+req.session.pid);
@@ -251,40 +311,16 @@ router.post('/projectaddtask', function(req, res, next) {
 router.route('/projecttaskedit')
     .get(function(req, res){
         let taskid = req.body.tid
-        console.log(taskid)
-
     })
     .post(function(req, res){
-
+        let taskid = req.body.tid
     })
 
+// #########################################################
+// FUND REQUEST PART
+// #########################################################
 
-router.get('/projectaddfundreq', function(req, res, next) {
-    if(req.session.user){
-        res.render('main/projects/projectaddfundreq',{csrfToken:req.csrfToken(),name:req.session.user});
-    }else{
-        res.redirect('login')
-    }
-});
-router.post('/projectaddfundreq', function(req, res, next) {
-    let rb = req.body
-    if(req.session.user){
-        let sql = "INSERT INTO finance.project_fundreq(projectid, title, detail, cost, creator)VALUES(?,?,?,?,?);"
-        let params = [req.session.pid, rb.title, rb.detail, rb.cost, req.session.idn];
-        console.log(params);
-        connection.query(sql,params,function (err, results, fields) {
-            if(err){
-                console.log(err);
-            }else{
-                res.redirect('/projectdetail?pid='+req.session.pid);
-            }
-        });
-    }else{
-        res.redirect('login')
-    }
-});
-
-//request fund here
+//fund detail // mysql
 router.get('/projectfunddetail', function(req, res, next){
     let fundid = req.query.fundid
     req.session.fid = fundid
@@ -295,7 +331,7 @@ router.get('/projectfunddetail', function(req, res, next){
                 ON project_member.memberid = account.number where projectid = ?;
                 SELECT * FROM project_fund_comment WHERE fundid = ?`
         connection.query(sql, [fundid, pid, fundid], function (err,results, fields){
-            res.render('main/projects/projectfunddetail',{
+            res.render('main/projects/fund/projectfunddetail',{
                 fund: results[0][0], pmember: results[1],
                 csrfToken:req.csrfToken(), comments:results[2], name:req.session.user
             });
@@ -304,6 +340,35 @@ router.get('/projectfunddetail', function(req, res, next){
         res.redirect('login')
     }
 });
+
+// add fund request
+router.route('/projectaddfundreq')
+    .get(function(req, res, next) {
+        if(req.session.user){
+            res.render('main/projects/fund/projectaddfundreq',{csrfToken:req.csrfToken(),name:req.session.user});
+        }else{
+            res.redirect('login')
+        }
+    })
+    .post(function(req, res, next) {
+        let rb = req.body
+        if(req.session.user){
+            let sql = "INSERT INTO finance.project_fundreq(projectid, title, detail, cost, creator)VALUES(?,?,?,?,?);"
+            let params = [req.session.pid, rb.title, rb.detail, rb.cost, req.session.idn];
+            console.log(params);
+            connection.query(sql,params,function (err, results, fields) {
+                if(err){
+                    console.log(err);
+                }else{
+                    res.redirect('/projectdetail?pid='+req.session.pid);
+                }
+            });
+        }else{
+            res.redirect('login')
+        }
+    })
+
+//fund comment add // mysql
 router.post('/fundaddcomment', function(req, res, next) {
     let rb = req.body
     if(req.session.user){
@@ -330,57 +395,9 @@ router.post('/fundaddcomment', function(req, res, next) {
     }
 });
 
-router.get('/projectaddmember', function(req, res, next) {
-    if(req.session.user){
-        let id = req.query.memberid
-        if(!id) id = '';
-        let sql = "SELECT number, id, name FROM nodedb.account where id = ?;"
-        connection.query(sql,id,function (err, result, fields) {
-            if (err) {console.log(err);
-            } else {
-                res.render('main/projects/projectaddmember', {id, result,csrfToken:req.csrfToken(), name: req.session.user});
-            }
-        });
-    }else{
-        res.redirect('login')
-    }
-});
-router.post('/projectaddmember', function(req, res, next) {
-    if(req.session.user){
-        let rb = req.body
-        let sql = "SELECT * FROM project_member WHERE projectid = ? AND memberid = ?"
-        let params = [req.session.pid, rb.id]
-        connection.query(sql,params,function (err, results, fields) {
-            if(err) {
-                throw err;
-            }else if(results.length === 0){
-                console.log(results.length)
-                sql = "INSERT INTO project_member(projectid, memberid)VALUES(?,?);"
-                params = [req.session.pid, rb.id];
-                connection.query(sql,params,function (err, results, fields) {
-                    if(err){
-                        console.log(err);
-                    }else{
-                        res.redirect('/projectdetail?pid='+req.session.pid);
-                    }
-                });
-            }else{
-                res.redirect('/projectdetail?pid='+req.session.pid);
-            }
-        });
-    }else{
-        res.redirect('login')
-    }
-});
-
-router.get('/projectadd', function (req, res,next){
-    if(req.session.user){
-        res.render('main/compara/underconstruction',{name:req.session.user});
-    }else{
-        res.redirect('login')
-    }
-})
-
+// #########################################################
+// UNDER CONSTRUCTION
+// #########################################################
 
 router.get('/analysis', function (req, res,next){
     if(req.session.user){
@@ -389,6 +406,5 @@ router.get('/analysis', function (req, res,next){
         res.redirect('login')
     }
 })
-
 
 module.exports = router;
