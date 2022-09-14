37b9a37d1f388fac4a19aeb6f05ef131f77eab11
Puri12 <tkdqja9812@gmail.com>
2022-09-15 00:26:10 +0900

    DB 관련 설정항목 env 적용

---
+++ routes/config/mysql.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql');
+require('dotenv').config();
+
+module.exports = function () {
+    return {
+        init: function () {
+            return mysql.createConnection({
+                host: process.env.MYSQL_HOST,
+                port: process.env.MYSQL_PORT,
+                user: process.env.MYSQL_USER,
+                password: process.env.MYSQL_PASSWD,
+                database: process.env.MYSQL_DB,
+                multipleStatements: true
+            })
+        },
+        db_open: function (con) {
+            con.connect(function (err) {
+                if (err) {
+                    console.error('mysql connection error :' + err);
+                } else {
+                    console.info('mysql is connected successfully.');
+                }
+            })
+        }
+    }
+};
