6d65881f04410b008f285d9894e286ef33d756a2
D3SP3R4D0S <90773781+D3SP3R4D0S@users.noreply.github.com>
2023-01-05 22:28:02 +0900

    [feat] apply knex on expendables

--- routes/expendables.js
+++ routes/expendables.js
@@ -1,27 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const mysql      = require('./config/mysql.js')();
-const connection = mysql.init();
 const knex = require('./config/knex.js');
-connection.connect(function(err){
-    if(err) {                                     // or restarting (takes a while sometimes).
-        console.log('error when connecting to db:', err);
-        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }
-});
-connection.close
 
 //expendables
 router.get('/expendables', function (req, res, next){
     if(req.session.user){
-        let sql = 'SELECT * FROM expendables WHERE userid = ?;'
-        let val = [req.session.idn]
-        connection.query(sql, val, function (err, results, fields) {
-            if(err) throw err;
-            else{
-                res.render('main/finance/expendables', {csrfToken:req.csrfToken(),result:results, name:req.session.user});
-            }
-        });
+        knex.select('*')
+            .from('expendables')
+            .where('userid', req.session.idn)
+            .then((result)=>{
+                res.render('main/finance/expendables', {csrfToken:req.csrfToken(), result, name:req.session.user});
+            })
+            .catch((err)=>{console.log(err)})
     }else{
         res.redirect('login')
     }
@@ -41,12 +32,25 @@ router.post('/expendablepurchaseadd', function (req, res, next){
         let rb = req.body
         let cost = rb.cost.replace(/,/g, '')
         let datetime = rb.date
-        let sql = "INSERT INTO finance.account(title, cost, detail, time, alligner, subord, userid)VALUES(?,?,?,?,?,?,?);"
-        let params = [rb.title, cost, rb.details, rb.date, rb.alligner, rb.subord, req.session.idn];
-        connection.query(sql,params,function (err) { if(err) console.log(err);});
-        sql = "UPDATE expendables SET `cost` = ?, `lastbought` = ? WHERE (`id` = ?);"
-        params = [cost, datetime, req.session.expendableitemid];
-        connection.query(sql,params,function (err) { if(err) console.log(err);});
+        knex.insert({
+                title:rb.title,
+                cost:cost,
+                detail:rb.details,
+                time:rb.date,
+                alligner:rb.alligner,
+                subord:rb.subord,
+                userid:req.session.idn
+            })
+            .into('account')
+            .then((result)=>{console.log(result)})
+            .catch((err)=>{console.log(err)})
+        knex('expendables').update({
+                cost:cost,
+                lastbought:datetime,
+            })
+            .where('id', req.session.expendableitemid)
+            .then((result)=>{console.log(result)})
+            .catch((err)=>{console.log(err)})
         res.redirect('/latestdata');
     }else{
         res.redirect('login')
@@ -66,12 +70,14 @@ router.post('/expendableedit', function (req, res){
 router.post('/expendableeditapply', function (req, res, next){
     if(req.session.user){
         let rb = req.body
-        let title = rb.title
-        let description = rb.description
-        let link = rb.link
-        sql = "UPDATE expendables SET `title` = ?, `description` = ?, `link` = ? WHERE (`id` = ?);"
-        params = [title, description, link, req.session.expendableitemid];
-        connection.query(sql,params,function (err) { if(err) console.log(err);});
+        knex('expendables').update({
+                title:rb.title,
+                description:rb.description,
+                link:rb.link,
+            })
+            .where('id', req.session.expendableitemid)
+            .then((result)=>{console.log(result)})
+            .catch((err)=>{console.log(err)})
         res.redirect('/expendables');
     }else{
         res.redirect('login')
@@ -85,12 +91,17 @@ router.get('/expendableadd', function(req, res, next) {
     }
 });
 router.post('/expendableadd', function(req, res, next) {
-    let rb = req.body;
-    let cost = rb.cost.replace(',', '')
     if(req.session.user){
-        let sql = "INSERT INTO expendables (userid, title, cost, link, description) VALUES (?, ?, ?, ?, ?);"
-        let params = [req.session.idn, rb.title, cost, rb.link, rb.description];
-        connection.query(sql,params,function (err, results, fields) {if(err){console.log(err);}});
+        knex.insert({
+                userid:req.session.idn,
+                title:req.body.title,
+                cost:req.body.cost.replace(',', ''),
+                link:req.body.link,
+                description:req.body.description
+            })
+            .into('expendables')
+            .then((result)=>{console.log(result)})
+            .catch((err)=>{console.log(err)})
         res.redirect('/expendables');
     }else{
         res.redirect('login')
