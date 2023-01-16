24aff953ea2a49b46cea9b3dcd1bff826b4614f2
D3SP3R4D0S <90773781+D3SP3R4D0S@users.noreply.github.com>
2023-01-17 00:10:12 +0900

    [feat] add default payment method

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,35 @@ const express = require('express');
 const router = express.Router();
 const knex = require('./config/knex.js');
 
-
+router.route('/personal_info')
+    .get(function(req, res){
+        console.log(req.session.idn)
+        knex.select('*')
+            .from('finance.personal_info')
+            .where('userid', req.session.idn)
+            .then((result)=>{
+                if(result.length == 0){
+                    knex('personal_info').insert({'userid':req.session.idn})
+                        .then((result)=>{console.log(result)                        })
+                        .catch((err)=>{console.log(err)})
+                }
+                res.status(200).json(result)
+            })
+            .catch((err)=>{
+                console.log(err)
+                res.status(500).json(err)
+            })
+    })
+    .post(function(req, res){
+        knex('finance.personal_info').update({
+            'default_method':req.body.default_method,
+            'default_usage':req.body.default_usage,
+            'default_alligner':req.body.default_alligner
+        })
+            .then((result)=>{
+                console.log(result)
+            })
+            .catch((err)=>console.log(err))
+    })
 
 module.exports = router;
